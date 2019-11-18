a5040c3fc30f14b5ddff3e9b05352e21be839058
attari1235 <attari1235@gmail.com>
2019-11-18 15:59:53 +0500

    Notification page made

--- screens/Notifications.js
+++ screens/Notifications.js
@@ -5,14 +5,8 @@ import {
   View,
   TouchableOpacity,
   Text,
-  FlatList,
-  ScrollView,
-  Image
+  Switch
 } from 'react-native'
-import CustomInput from '../Component/Input'
-import ProductDescription from '../Component/ProductDescription'
-import CustomButton from '../Component/Button'
-import CustomHeader from '../Component/header'
 import { withNavigation, NavigationEvents } from 'react-navigation'
 import { Icon } from 'react-native-elements'
 import { themeColor } from '../Constant'
@@ -23,23 +17,48 @@ const url =
 class Cart extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      notification : false,
+      emailNotification : false
+    }
   }
   static navigationOptions = {
     header: null
   }
 
+  _Icon = (name) =>
+  <TouchableOpacity >
+  <Icon type = {'font-awesome'} name = {name} 
+  color = {themeColor} size = {26} 
+  containerStyle = {styles.buttonContainer} />
+  </TouchableOpacity>
+
   render () {
     const { navigation } = this.props
+    const {notification , emailNotification} = this.state
     return (
       <View style={{ flex: 1 }}>
         <CustomHeader home title={'NOTIFICATIONS'} />
-        <View style = {{flexDirection : 'row' , marginHorizontal : 12 , alignItems : 'center' , minHeight : 80}}>
-            <Text style = {{fontSize : 18 , color : themeColor , fontWeight : 'bold'}}>Sound </Text>
+        <View style = {[styles.rowContainer , {borderBottomColor : themeColor , borderBottomWidth : 1}]}>
+            <Text style = {styles.heading}>Sound </Text>
             <View style = {{flexDirection : 'row'}}>
-                <Icon type = {'font-awesome'} name = {'volume-up'} color = {themeColor} />
-                <Icon type = {'font-awesome'} name = {'volume-mute'} color = {themeColor} />
-                <Icon type = {'font-awesome'} name = {'mobile'} color = {themeColor} />
+              {this._Icon("volume-up")}
+              {this._Icon("volume-off")}
+              {this._Icon("mobile")}
+             </View>
+            </View>
+            <View style = {[styles.rowContainer , {borderBottomColor : themeColor , borderBottomWidth : 1}]}>
+            <Text style = {styles.heading}>Notification </Text>
+            <View style = {{flexDirection : 'row'}}>
+              <Switch  value = {notification}  thumbColor  ={themeColor} 
+              trackColor = {{true : 'skyblue' , false : '#ccc'}}onValueChange = {()=> this.setState({notification : !notification})}/>
+             </View>
+            </View>
+            <View style = {[styles.rowContainer , {borderBottomColor : themeColor , borderBottomWidth : 1}]}>
+            <Text style = {styles.heading}>Email Notification </Text>
+            <View style = {{flexDirection : 'row'}}>
+              <Switch  value = {emailNotification}  thumbColor  ={themeColor} 
+              trackColor = {{true : 'skyblue' , false : '#ccc'}}onValueChange = {()=> this.setState({emailNotification : !emailNotification})}/>
              </View>
             </View>
       </View>
@@ -48,8 +67,12 @@ class Cart extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
+  container: { flex: 1},
+  rowContainer : {flexDirection : 'row' , paddingHorizontal : 12 , alignItems : 'center' , minHeight : 90 , justifyContent : 'space-between'},
+  heading : {fontSize : 18 , color : themeColor , fontWeight : 'bold'},
+  buttonContainer : {marginHorizontal : 2 , backgroundColor : "#D8F0FF" ,
+  borderRadius : 25 , height : 45 , width : 45, justifyContent : "center"},
+
 })
 export default withNavigation(Cart)
+ 
